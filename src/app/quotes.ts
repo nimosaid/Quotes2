9dd380eeae48308f4452bce8c337a547110c5cbf
nimosaid <saidnimo123@gmail.com>
2019-07-05 12:50:08 +0300

    add changes

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,5 @@
 export class Quotes {
+  public showAuthor:boolean;
+   constructor(public id:number, public name:string,public author:string,public submitted:string){
+       this.showAuthor=false
 }

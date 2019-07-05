657f10626d87775bd7c35c65f72daaa815bc5578
nimosaid <saidnimo123@gmail.com>
2019-07-05 12:42:54 +0300

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Quotes } from './quotes';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // quotes: Quotes = [
-  //   {name:'Keeper',vote:23},
-  //   {name:'Lover',vote:21},
-  //   {name:'Roller',vote:12},
-  // ];
-  // costructor(){
-  //   this.quotes = ['You Only Live Once' ,'Charity starts at home']
-  // }
+  quotes = [
+    new Quote(1, 'The sun is a star.','A.B Clark',"Z.G"),
+    new Quote(2, 'The moon is beautiful.', 'B.C Smith',"E.I"),
+    new Quote(3, 'The earth is meh.', 'D.E Jacobsons',"T.O"),
+    new Quote(4, 'The galaxy is large.', 'F.G Randos',"P.S"),
+    new Quote(5, 'The universe is expanding.', 'I.J Persons',"Q.T"),
+];
+  constructor(){  }
 }
